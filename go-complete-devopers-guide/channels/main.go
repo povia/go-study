eac5597813a94a8aa5c037a1bae69814bca7b61d
povia <47127607+povia@users.noreply.github.com>
2023-05-21 02:01:14 +0900

    Go The Complete Developer's Guide until 76

--- go-complete-devopers-guide/channels/main.go
+++ go-complete-devopers-guide/channels/main.go
@@ -14,16 +14,23 @@ func main() {
 		"https://golang.org",
 		"https://amazon.com",
 	}
+
+	c := make(chan string)
 	for _, link := range links {
-		go CheckLink(link)
+		go CheckLink(link, c)
 	}
+
+	fmt.Println(<-c)
 }
 
-func CheckLink(link string) {
+func CheckLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		c <- "Might be down I think"
 		os.Exit(1)
+
 	}
+	c <- "Yep its up"
 	fmt.Println(link, "might be up!")
 }
